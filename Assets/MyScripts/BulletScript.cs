c02038df4726aa4e28908ce50b2bba94324e3921
Anthony Ramosleborn <ajramoslebron@gmail.com>
2017-09-16 13:32:47 -0400

    Created small scene with a globe and a ship hub

---
+++ Assets/MyScripts/BulletScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour {
+
+    public float speed = 3;
+    public int Dies = 5;
+    private float timer = 0;
+    // Use this for initialization
+    void Start () {
+        
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+
+        if(timer <= Dies)
+        {
+            timer += Time.deltaTime;
+            if(timer >= Dies)
+                Destroy(gameObject);
+          
+        }
+    }
+}
