aaae1f387b94a75f932281686220cf920a93d774
Anthony Ramosleborn <ajramoslebron@gmail.com>
2017-09-16 16:47:56 -0400

    Finished adding shaking feature ading aliens to screen

---
+++ Assets/MyScripts/TouchInput.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchInput : MonoBehaviour {
+
+    public LayerMask touchInputMask;
+
+    private List<GameObject> touchlist = new List<GameObject>();
+    private GameObject[] touchesOld;
+    private RaycastHit hit;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+#if UNITY_EDITOR
+
+        if (Input.GetMouseButton(0) || Input.GetMouseButtonDown(0) || Input.GetMouseButtonUp(0))
+        {
+            touchesOld = new GameObject[touchlist.Count];
+            touchlist.CopyTo(touchesOld);
+            touchlist.Clear();
+            
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+
+            if (Physics.Raycast(ray, out hit, touchInputMask))
+            {
+                GameObject recipient = hit.transform.gameObject;
+                touchlist.Add(recipient);
+                if (Input.GetMouseButtonDown(0))
+                {
+                    recipient.SendMessage("OnTouchDown", hit.point, SendMessageOptions.DontRequireReceiver);
+                }
+                if (Input.GetMouseButtonUp(0))
+                {
+                    recipient.SendMessage("OnTouchUp", hit.point, SendMessageOptions.DontRequireReceiver);
+                }
+                if (Input.GetMouseButton(0))
+                {
+                    recipient.SendMessage("OnTouchStay", hit.point, SendMessageOptions.DontRequireReceiver);
+                }
+                
+            }
+            
+            foreach (var g in touchesOld)
+            {
+                if (!touchlist.Contains(g))
+                    g.SendMessage("OnTouchExit", hit.point, SendMessageOptions.DontRequireReceiver);
+            }
+        }
+#endif
+
+        if (Input.touchCount > 0)
+        {
+            touchesOld = new GameObject[touchlist.Count];
+            touchlist.CopyTo(touchesOld);
+            touchlist.Clear();
+            foreach (Touch touch in Input.touches)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                
+
+                if (Physics.Raycast(ray, out hit, touchInputMask))
+                {
+                    GameObject recipient = hit.transform.gameObject;
+                    touchlist.Add(recipient);
+                    if (touch.phase == TouchPhase.Began)
+                    {
+                        recipient.SendMessage("OnTouchDown", hit.point, SendMessageOptions.DontRequireReceiver);
+                    }
+                    if (touch.phase == TouchPhase.Ended)
+                    {
+                        recipient.SendMessage("OnTouchUp", hit.point, SendMessageOptions.DontRequireReceiver);
+                    }
+                    if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
+                    {
+                        recipient.SendMessage("OnTouchStay", hit.point, SendMessageOptions.DontRequireReceiver);
+                    }
+                    if (touch.phase == TouchPhase.Canceled)
+                    {
+                        recipient.SendMessage("OnTouchExit", hit.point, SendMessageOptions.DontRequireReceiver);
+                    }
+                }
+            }
+            foreach (var g in touchesOld)
+            {
+                if(!touchlist.Contains(g))
+                    g.SendMessage("OnTouchExit", hit.point, SendMessageOptions.DontRequireReceiver);
+            }
+        }
+		
+	}
+}
